8db85b207ca628c0d6f37d5d4c26e25de9342c56
chunhodong <yhms4432@naver.com>
2022-08-08 00:07:36 +0900

    [feat] querydsl설정,페이징클래스추가

--- boiler/src/main/java/com/bronze/boiler/filter/Page.java
+++ boiler/src/main/java/com/bronze/boiler/filter/Page.java
@@ -11,4 +11,13 @@ public class Page {
 
     private Long pageNum;
     private Long pageSize;
+    private Long pageOffset;
+
+    public Long getPageOffset(){
+        return pageNum > 0 ? ( pageNum - 1 ) * pageSize : 0;
+    }
+
+    public Long getPageSize(){
+        return pageSize == null ? 10L : pageSize;
+    }
 }
--- boiler/src/main/java/com/bronze/boiler/repository/ProductRepositoryImpl.java
+++ boiler/src/main/java/com/bronze/boiler/repository/ProductRepositoryImpl.java
@@ -2,15 +2,23 @@ package com.bronze.boiler.repository;
 
 import com.bronze.boiler.domain.product.entity.Product;
 import com.bronze.boiler.filter.Page;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-
 import java.util.List;
+import static com.bronze.boiler.domain.product.entity.QProduct.product;
 
 @RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCst{
 
+    private final JPAQueryFactory queryFactory;
+
     @Override
     public List<Product> findAllByPage(Page page) {
-        return null;
+
+        return queryFactory
+                .selectFrom(product)
+                .offset(page.getPageOffset())
+                .limit(page.getPageSize())
+                .fetch();
     }
 }

2184c2af566c66cd22adeffd67cc3bbadce6ac6f
chunhodong <yhms4432@naver.com>
2022-09-15 23:09:18 +0900

    [feat] 엔티티에 BaseDate상속
    테이블 날짜필드추가

---
+++ boiler/src/main/generated/com/bronze/boiler/domain/payment/entity/QPayment.java
@@ -0,0 +1,45 @@
+package com.bronze.boiler.domain.payment.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPayment is a Querydsl query type for Payment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPayment extends EntityPathBase<Payment> {
+
+    private static final long serialVersionUID = -658107453L;
+
+    public static final QPayment payment = new QPayment("payment");
+
+    public final com.bronze.boiler.domain.base.QBaseDate _super = new com.bronze.boiler.domain.base.QBaseDate(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> created = _super.created;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modified = _super.modified;
+
+    public QPayment(String variable) {
+        super(Payment.class, forVariable(variable));
+    }
+
+    public QPayment(Path<? extends Payment> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPayment(PathMetadata metadata) {
+        super(Payment.class, metadata);
+    }
+
+}
+
